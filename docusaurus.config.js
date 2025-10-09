0bf2a8c0ca15e92069309a847e39b917df4c5476
xenthrall <xenthrall0@gmail.com>
2025-10-09 18:51:42 -0500

    feat: agrega configuracion, metadatos y una lista de caracteristicas

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'xenthrall', // Usually your GitHub org/user name.
+  projectName: 'CronoSENA', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -36,8 +36,8 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'es',
+    locales: ['es'],
   },
 
   presets: [
@@ -50,8 +50,9 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/xenthrall/CronoSENA',
         },
+        
         blog: {
           showReadingTime: true,
           feedOptions: {
@@ -61,7 +62,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/xenthrall/CronoSENA',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -83,9 +84,9 @@ const config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'CronoSENA Docs',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'CronoSENA Logo',
           src: 'img/logo.svg',
         },
         items: [
@@ -116,19 +117,19 @@ const config = {
             ],
           },
           {
-            title: 'Community',
+            title: 'Contacto',
             items: [
               {
                 label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                href: '',
               },
               {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                href: '',
               },
               {
                 label: 'X',
-                href: 'https://x.com/docusaurus',
+                href: '',
               },
             ],
           },
@@ -141,12 +142,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/xenthrall/CronoSENA',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} CronoSENA, xenthrall. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,35 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Gestión Inteligente de Formación',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        CronoSENA centraliza la planificación académica del SENA, optimizando la
+        asignación de instructores, ambientes y fichas para lograr una gestión
+        precisa y eficiente.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Hecho para la Comunidad Educativa',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Diseñado para administradores, instructores y aprendices, CronoSENA
+        simplifica los procesos diarios y mejora la comunicación entre todos los
+        actores de la formación profesional.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Potenciado por Tecnología Moderna',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Desarrollado con <b>Laravel</b> y <b>Filament</b>, CronoSENA combina la
+        estabilidad del backend con la flexibilidad del frontend, ofreciendo una
+        experiencia fluida, segura y escalable.
       </>
     ),
   },
